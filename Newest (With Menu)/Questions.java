7cc366bd76bd6d2cb239b3d34f7fea0fcca7c064
joemerk <32776593+joemerk@users.noreply.github.com>
2018-04-20 22:29:33 +0100

    added menu/changed some code
    Some classes changed so they would be compatible with menu.

---
+++ Newest (With Menu)/Questions.java
@@ -0,0 +1,91 @@
+import java.util.Random;
+import java.io.*;
+import java.util.ArrayList;
+
+class Questions {
+	static ArrayList<Integer> ListOfAnsweredQuestions = new ArrayList<>();
+	static int SizeOfFile = 0;
+
+	public Questions( String title, String barcode) {
+	}
+
+  public static void SetSizeOfFile(){
+    String FileName = "Questions.txt";
+    int Count = 1;
+    String CurrentLineToBeRead;
+    try {
+      FileInputStream fis = new FileInputStream(FileName);
+      BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+      while((CurrentLineToBeRead = br.readLine()) != null){
+        Count = Count + 1;
+      }
+        br.close();
+    }catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    //System.out.println("Count is " + Count);
+    SizeOfFile = Count;
+  }
+
+	public static void AddQuestion( String[] QuestionDetails ) {
+      String FileName = "Questions.txt";
+      String DataToBeWritten;
+
+      try{
+      BufferedWriter bw = new BufferedWriter(new FileWriter(FileName, true));
+        for (int i = 0; i < QuestionDetails.length; ++i) {
+          DataToBeWritten = QuestionDetails[i];
+          bw.write(DataToBeWritten + ",");   
+          bw.flush();
+        }
+        bw.newLine();
+        bw.flush();
+        bw.close();
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+        SizeOfFile = SizeOfFile + 1;
+    }
+
+     public static String[] getRandomUnansweredQuestion( ){
+      	String FileName = "Questions.txt";
+      	String CurrentLineToBeRead;
+      	String[] QuestionReturned = new String[] {"","","","","","",""}; //had to predeclare these values because of indexing issue
+      	int LineNumber;
+        boolean endOfQuestions = false;
+
+        do{
+          //QuestionReturned = new String[] {}; 
+          Random Random = new Random();
+      		do{
+      		LineNumber = Random.nextInt(SizeOfFile) + 1;
+      		}while(ListOfAnsweredQuestions.contains(LineNumber) != false);
+
+          try {
+            FileInputStream fis = new FileInputStream(FileName);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+
+            int Count = 1;
+            while((CurrentLineToBeRead = br.readLine()) != null){
+              if (Count == LineNumber){
+                QuestionReturned = CurrentLineToBeRead.split(",");
+              }
+              Count = Count + 1;
+            }
+
+            br.close();
+          } catch (Exception e) {
+              e.printStackTrace();
+          }
+        } while(QuestionReturned[6].equals(QuizPresets.getChoice()) == false);
+
+        ListOfAnsweredQuestions.add(LineNumber);
+        return QuestionReturned;
+  }
+
+  public static void resetListOfAnsweredQuestions( ){
+    ListOfAnsweredQuestions.clear();
+  }
+}
