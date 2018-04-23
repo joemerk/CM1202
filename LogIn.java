8046237f09fd049c2a0d4f683c0c69a0c204cb6e
JoeDinn <joedinn1@gmail.com>
2018-04-23 11:51:02 +0000

    updated

--- LogIn.java
+++ LogIn.java
@@ -1,11 +1,28 @@
 import java.security.MessageDigest;
 public class LogIn {
 	private String adminPassword;
-	private String hashPassword(){
+	private String hashPassword(String password){
 		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
-		messageDigest.update(stringToEncrypt.getBytes());
+		messageDigest.update(password.getBytes());
 		String encryptedString = new String(messageDigest.digest());
 		return encryptedString;
 	}
-
+	public String changePassword(){
+		if (adminPassword == null){
+			adminPassword = hashPassword(GetNewPassword());
+			return "Success";
+		}else if (verifyPassword()){
+			adminPassword = hashPassword(GetNewPassword());
+			return "Success";
+		}else{
+			return "Incorrect password";
+		}
+	}
+	//private String GetNewPassword(){
+		//get password
+	//	return;
+	//}
+	private boolean verifyPassword(String password){
+		return hashPassword(password).equals(adminPassword);
+	}
 }
